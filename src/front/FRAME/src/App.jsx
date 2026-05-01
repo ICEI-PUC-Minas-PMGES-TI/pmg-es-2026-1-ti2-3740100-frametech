6f6f9ea6ddde4e2892fff9745b214657e1d9d0c8
Mateus Ribas <marcalmateus315@gmail.com>
2026-05-01 10:53:31 -0300

    feat: Tela Login

--- src/front/FRAME/src/App.jsx
+++ src/front/FRAME/src/App.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home/Home";
 import Header from "./Components/Header/Header";
 import Cadastro from "./Pages/Cadastro/Cadastro";
+import Login from "./Pages/Login/Login";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Cadastro" element={<Cadastro />} />
+        <Route path="/Login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
