35fde9de8dec26614c28bf9cd633ac0dbbd13d65
Gustavo Paulino Cicero <gugupaulino123@gmail.com>
2026-05-04 15:49:20 -0300

    feat: corrige rotas do App.jsx e integra HomeClient

--- src/front/FRAME/src/App.jsx
+++ src/front/FRAME/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./Pages/Home/Home";
 import Cadastro from "./Pages/Cadastro/Cadastro";
 import Login from "./Pages/Login/Login";
@@ -9,11 +9,23 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Rota Raiz: Landing Page */}
         <Route path="/" element={<Home />} />
+        
+        {/* Rotas de Autenticação */}
         <Route path="/cadastro" element={<Cadastro />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/home-cliente" element={<HomeClient />} />
+        
+        {/* Rota do Dashboard (Onde o "Pedro" aparece) */}
+        {/* Mudamos de /home-cliente para /client para bater com o seu teste */}
+        <Route path="/client" element={<HomeClient />} />
+        
+        {/* Rota de Perfil */}
         <Route path="/perfil" element={<Perfil />} />
+
+        {/* Redirecionamento de segurança: 
+            Se digitar qualquer coisa errada, volta para a Home */}
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </BrowserRouter>
   );
