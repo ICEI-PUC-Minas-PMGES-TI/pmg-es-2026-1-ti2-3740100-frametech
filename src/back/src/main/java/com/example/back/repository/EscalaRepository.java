1e60c9dcf032ce5b0892e3832696f82115794be9
llucasBraz <164803203+llucasBraz@users.noreply.github.com>
2026-05-25 20:21:03 -0300

    Update EscalaRepository.java

--- src/back/src/main/java/com/example/back/repository/EscalaRepository.java
+++ src/back/src/main/java/com/example/back/repository/EscalaRepository.java
@@ -2,12 +2,12 @@ package com.example.back.repository;
 
 import com.example.back.model.Escala;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface EscalaRepository extends JpaRepository<Escala, Long> {
-    // Caso queira buscar depois todas as escalas de um evento específico
-    List<Escala> findByEvento_Id(Long eventoId);
+
+    List<Escala> findByProfissional_Id(Long profissionalId);
+
+    List<Escala> findByProfissional_IdAndStatus(Long profissionalId, String status);
 }
