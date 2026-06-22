2dacf283f9c9d47314fc9581464b45c849b7fdd9
Mateus Ribas <marcalmateus315@gmail.com>
2026-06-22 15:27:00 -0300

    atualizacao proprietes

--- src/back/src/main/java/com/example/back/PerfilApiApplication.java
+++ src/back/src/main/java/com/example/back/PerfilApiApplication.java
@@ -2,8 +2,10 @@ package com.example.back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.example.back")
 public class PerfilApiApplication {
 
     public static void main(String[] args) {
