45f0efab740fe0638baac5acc55cb1c7ef06d835
Mateus Ribas <marcalmateus315@gmail.com>
2026-06-28 14:58:50 -0300

    atualizacao rotas main

--- src/front/FRAME/src/App.jsx
+++ src/front/FRAME/src/App.jsx
@@ -5,24 +5,28 @@ import {
     Navigate
 } from "react-router-dom";
 
-import Home from "./PAGES/HOME/Home";
-import Cadastro from "./PAGES/Cadastro/Cadastro";
-import Login from "./PAGES/Login/Login";
-import HomeClient from "./PAGES/HomeClient/HomeClient";
-import HomeProfissional from "./PAGES/HomeProfissional/HomeProfissional";
-import HomeAdm from "./PAGES/HomeAdm/Homeadm";
-import Perfil from "./PAGES/Perfil/Perfil";
-import Eventos from "./PAGES/Eventos/Eventos";
-import EscalaEquipe from "./PAGES/Escalas/EscalaEquipe";
-import GestaoEquipamentos from "./PAGES/EQUIPAMENTOS/GestaoEquipamentos";
-import ChatEventos from "./PAGES/ChatEventos/ChatEventos";
-import EsqueciSenha from "./PAGES/EsqueciSenha/EsqueciSenha";
+import { lazy, Suspense } from "react";
+
 import ProtectedRoute from "./ProtectedRoute";
 import ThemeToggle from "./Components/ThemeToggle/ThemeToggle";
+
 import {
     getCurrentUser,
     getHomeByRole
 } from "./utils/authRoutes";
+
+const Home = lazy(() => import("./PAGES/HOME/Home"));
+const Cadastro = lazy(() => import("./PAGES/Cadastro/Cadastro"));
+const Login = lazy(() => import("./PAGES/Login/Login"));
+const HomeClient = lazy(() => import("./PAGES/HomeClient/HomeClient"));
+const HomeProfissional = lazy(() => import("./PAGES/HomeProfissional/HomeProfissional"));
+const HomeAdm = lazy(() => import("./PAGES/HomeAdm/Homeadm"));
+const Perfil = lazy(() => import("./PAGES/Perfil/Perfil"));
+const Eventos = lazy(() => import("./PAGES/Eventos/Eventos"));
+const EscalaEquipe = lazy(() => import("./PAGES/Escalas/EscalaEquipe"));
+const GestaoEquipamentos = lazy(() => import("./PAGES/EQUIPAMENTOS/GestaoEquipamentos"));
+const ChatEventos = lazy(() => import("./PAGES/ChatEventos/ChatEventos"));
+const EsqueciSenha = lazy(() => import("./PAGES/EsqueciSenha/EsqueciSenha"));
 
 function RoleRedirect() {
     const { isAuthenticated, tipoUsuario } = getCurrentUser();
@@ -49,119 +53,123 @@ function App() {
     return (
         <BrowserRouter>
             <ThemeToggle />
-            <Routes>
 
-                <Route
-                    path="/"
-                    element={
-                        <PublicOnlyRoute>
-                            <Home />
-                        </PublicOnlyRoute>
-                    }
-                />
+            <Suspense fallback={<div>Carregando...</div>}>
+                <Routes>
 
-                <Route
-                    path="/cadastro"
-                    element={
-                        <PublicOnlyRoute>
-                            <Cadastro />
-                        </PublicOnlyRoute>
-                    }
-                />
+                    <Route
+                        path="/"
+                        element={
+                            <PublicOnlyRoute>
+                                <Home />
+                            </PublicOnlyRoute>
+                        }
+                    />
 
-                <Route
-                    path="/login"
-                    element={
-                        <PublicOnlyRoute>
-                            <Login />
-                        </PublicOnlyRoute>
-                    }
-                />
+                    <Route
+                        path="/cadastro"
+                        element={
+                            <PublicOnlyRoute>
+                                <Cadastro />
+                            </PublicOnlyRoute>
+                        }
+                    />
 
-                <Route
-                    path="/esqueci-senha"
-                    element={
-                        <PublicOnlyRoute>
-                            <EsqueciSenha />
-                        </PublicOnlyRoute>
-                    }
-                />
+                    <Route
+                        path="/login"
+                        element={
+                            <PublicOnlyRoute>
+                                <Login />
+                            </PublicOnlyRoute>
+                        }
+                    />
 
-                <Route
-                    path="/home-cliente"
-                    element={
-                        <ProtectedRoute permitido="cliente">
-                            <HomeClient />
-                        </ProtectedRoute>
-                    }
-                />
+                    <Route
+                        path="/esqueci-senha"
+                        element={
+                            <PublicOnlyRoute>
+                                <EsqueciSenha />
+                            </PublicOnlyRoute>
+                        }
+                    />
 
-                <Route
-                    path="/eventos"
-                    element={
-                        <ProtectedRoute permitido="cliente">
-                            <Eventos />
-                        </ProtectedRoute>
-                    }
-                />
+                    <Route
+                        path="/home-cliente"
+                        element={
+                            <ProtectedRoute permitido="cliente">
+                                <HomeClient />
+                            </ProtectedRoute>
+                        }
+                    />
 
-                <Route
-                    path="/home-profissional"
-                    element={
-                        <ProtectedRoute permitido="prestador">
-                            <HomeProfissional />
-                        </ProtectedRoute>
-                    }
-                />
+                    <Route
+                        path="/eventos"
+                        element={
+                            <ProtectedRoute permitido="cliente">
+                                <Eventos />
+                            </ProtectedRoute>
+                        }
+                    />
 
-                <Route
-                    path="/home-adm"
-                    element={
-                        <ProtectedRoute permitido="adm">
-                            <HomeAdm />
-                        </ProtectedRoute>
-                    }
-                />
+                    <Route
+                        path="/home-profissional"
+                        element={
+                            <ProtectedRoute permitido="prestador">
+                                <HomeProfissional />
+                            </ProtectedRoute>
+                        }
+                    />
 
-                <Route
-                    path="/escalas"
-                    element={
-                        <ProtectedRoute permitido="adm">
-                            <EscalaEquipe />
-                        </ProtectedRoute>
-                    }
-                />
+                    <Route
+                        path="/home-adm"
+                        element={
+                            <ProtectedRoute permitido="adm">
+                                <HomeAdm />
+                            </ProtectedRoute>
+                        }
+                    />
 
-                <Route
-                    path="/equipamentos"
-                    element={
-                        <ProtectedRoute permitido="adm">
-                            <GestaoEquipamentos />
-                        </ProtectedRoute>
-                    }
-                />
+                    <Route
+                        path="/escalas"
+                        element={
+                            <ProtectedRoute permitido="adm">
+                                <EscalaEquipe />
+                            </ProtectedRoute>
+                        }
+                    />
 
-                <Route
-                    path="/chat-eventos"
-                    element={
-                        <ProtectedRoute permitido={["cliente", "prestador", "adm"]}>
-                            <ChatEventos />
-                        </ProtectedRoute>
-                    }
-                />
+                    <Route
+                        path="/equipamentos"
+                        element={
+                            <ProtectedRoute permitido="adm">
+                                <GestaoEquipamentos />
+                            </ProtectedRoute>
+                        }
+                    />
 
-                <Route
-                    path="/perfil"
-                    element={
-                        <ProtectedRoute permitido={["cliente", "prestador", "adm"]}>
-                            <Perfil />
-                        </ProtectedRoute>
-                    }
-                />
+                    <Route
+                        path="/chat-eventos"
+                        element={
+                            <ProtectedRoute permitido={["cliente", "prestador", "adm"]}>
+                                <ChatEventos />
+                            </ProtectedRoute>
+                        }
+                    />
 
-                <Route path="*" element={<RoleRedirect />} />
+                    <Route
+                        path="/perfil"
+                        element={
+                            <ProtectedRoute permitido={["cliente", "prestador", "adm"]}>
+                                <Perfil />
+                            </ProtectedRoute>
+                        }
+                    />
 
-            </Routes>
+                    <Route path="*" element={<RoleRedirect />} />
+
+                </Routes>
+            </Suspense>
+
         </BrowserRouter>
     );
 }
